43a1637da81594857761905d697af6e79371a630
Alexandre Gérault <alexandre@hexium.io>
2020-11-14 14:16:26 +0100

    feat: fake follow

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+
+const localSocket = io('http://localhost:1111')
 
 const App = () => {
   const [name, setUserName] = useState('ThomLec');
+
+  useEffect(() => {
+    localSocket.on('connect', () => {
+      console.log("Connected to local server")
+    })
+
+    localSocket.on('event', (payload) => {
+      console.log(payload)
+    })
+  })
 
   const handleInput = (event) => {
     setUserName(event.target.value)
   }
 
   const handleFollow = () => {
-    console.log("Handle follow")
+    localSocket.emit('event', {
+      type: "follow",
+      message: [
+        {
+          created_at: "2017-08-22 00:14:35",
+          id: "170884026",
+          name: "h4r5h48002",
+          _id: "74a0b93e736f1f14762111f8ae34bf42"
+        }
+      ],
+      for: "twitch_account"
+    })
   }
 
   const handleSub = () => {
